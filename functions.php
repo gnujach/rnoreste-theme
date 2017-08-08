4717a2ff81be8cb0dee4801ae418d6bb76612151
Jose Julian Abarca <julian.abarca@gmail.com>
2017-08-08 12:15:42 -0500

    Cambios en pagina de login

--- functions.php
+++ functions.php
@@ -85,3 +85,17 @@ function get_reconocimientos( $query ) {
 }
  	
 add_action( 'pre_get_posts', 'get_reconocimientos' );
+// Cambiar logo en la pagina de acceso de usuarios
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/site-login-logo.png);
+		height:164px;
+		width:300px;
+		background-size: 300px 164px;
+		background-repeat: no-repeat;
+        padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
