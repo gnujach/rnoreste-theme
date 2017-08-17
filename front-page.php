812b23338a4e1d1a3c5d4af42f3b125c38af0db4
gnujach <josejulianabarca@MacBook-Air-de-Jose.local>
2017-08-16 20:55:05 -0500

    cambios en front-page agregamos enlaces

--- front-page.php
+++ front-page.php
@@ -21,8 +21,8 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
                 <img class="card-img-top" src="<?php echo get_theme_file_uri( '/img/alumnos.png' )?>" alt="Card image cap">
                 <div class="card-block">
                     <h3 class="card-title">Amor</h3>
-                    <p class="card-text">Descripcion del valor.</p>
-                    <a href="#" class="btn btn-primary">Vamos</a>
+                    <p class="card-text">Es un acto de voluntad  por el que libremente  se quiere y busca el bien  de otra persona..</p>
+                    <a href="amor/" class="btn btn-primary">Vamos</a>
                 </div>
             </div>
         </div>
@@ -31,8 +31,8 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
                 <img class="card-img-top" src="<?php echo get_theme_file_uri( '/img/alumnos.png' )?>" alt="Card image cap">
                 <div class="card-block">
                     <h3 class="card-title">Respeto</h3>
-                    <p class="card-text">Descripcion del valor.</p>
-                    <a href="#" class="btn btn-primary">Vamos</a>
+                    <p class="card-text">Es la base de la convivencia. Es valor inherente del ser humano, y de los derechos innatos de las personas.</p>
+                    <a href="respeto/" class="btn btn-primary">Vamos</a>
                 </div>
             </div>
         </div>
@@ -41,8 +41,8 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
                 <img class="card-img-top" src="<?php echo get_theme_file_uri( '/img/alumnos.png' )?>" alt="Card image cap">
                 <div class="card-block">
                     <h3 class="card-title">Benedicencia</h3>
-                    <p class="card-text">Descripcion del valor.</p>
-                    <a href="#" class="btn btn-primary">Vamos</a>
+                    <p class="card-text">expresar  y reconocer las cualidades y potencialidades  que tenemos las personas.</p>
+                    <a href="benedicencia" class="btn btn-primary">Vamos</a>
                 </div>
             </div>
         </div>        
@@ -53,8 +53,8 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
                 <img class="card-img-top" src="<?php echo get_theme_file_uri( '/img/alumnos.png' )?>" alt="Card image cap">
             <div class="card-block">
                 <h3 class="card-title">Honestidad</h3>
-                <p class="card-text">Descripcion del valor.</p>
-                    <a href="#" class="btn btn-primary">Vamos</a>
+                <p class="card-text">Vivir  congruentemente  entre lo que se  piensa  y lo que se demuestra  con nuestros actos  y los demás.</p>
+                    <a href="honestidad" class="btn btn-primary">Vamos</a>
             </div>
             </div>
         </div>
@@ -63,8 +63,8 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
                 <img class="card-img-top" src="<?php echo get_theme_file_uri( '/img/alumnos.png' )?>" alt="Card image cap">
             <div class="card-block">
                 <h3 class="card-title">Responsabilidad</h3>
-                <p class="card-text">Descripcion del valor.</p>
-                    <a href="#" class="btn btn-primary">Vamos</a>
+                <p class="card-text">DImplica tomar decisiones  libremente  aceptando las consecuencias, deberes y obligaciones.</p>
+                    <a href="responsabilidad" class="btn btn-primary">Vamos</a>
             </div>
             </div>
         </div>
@@ -73,8 +73,8 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
                 <img class="card-img-top" src="<?php echo get_theme_file_uri( '/img/alumnos.png' )?>" alt="Card image cap">
             <div class="card-block">
                 <h3 class="card-title">Generosidad</h3>
-                <p class="card-text">Descripcion del valor.</p>
-                    <a href="#" class="btn btn-primary">Vamos</a>
+                <p class="card-text">Es la  cualidad  para ser útil y caritativo/a. La persona generosa  es desprendida y sabe compartir.</p>
+                    <a href="generosidad" class="btn btn-primary">Vamos</a>
             </div>
             </div>
         </div>        
@@ -85,8 +85,8 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
                 <img class="card-img-top" src="<?php echo get_theme_file_uri( '/img/alumnos.png' )?>" alt="Card image cap">
             <div class="card-block">
                 <h3 class="card-title">Unión</h3>
-                <p class="card-text">Descripcion del valor.</p>
-                    <a href="#" class="btn btn-primary">Vamos</a>
+                <p class="card-text">La unidad de sustento, fuerza  y valor  para hacer  que lo imposible se haga posible</p>
+                    <a href="union/" class="btn btn-primary">Vamos</a>
             </div>
             </div>
         </div>
@@ -95,7 +95,7 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
                 <img class="card-img-top" src="<?php echo get_theme_file_uri( '/img/alumnos.png' )?>" alt="Card image cap">
             <div class="card-block">
                 <h3 class="card-title">Vive los valores</h3>
-                <p class="card-text">Descripcion del valor.</p>
+                <p class="card-text">Reconoce las buenas acciones de tus compañeros.</p>
                     <a href="#" class="btn btn-primary">Vamos</a>
             </div>
             </div>
@@ -105,7 +105,7 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
                 <img class="card-img-top" src="<?php echo get_theme_file_uri( '/img/alumnos.png' )?>" alt="Card image cap">
             <div class="card-block">
                 <h3 class="card-title">Noticias</h3>
-                <p class="card-text">Descripcion del valor.</p>
+                <p class="card-text">Conoce las acciones que estamos realizando en el comité de valores.</p>
                     <a href="#" class="btn btn-primary">Vamos</a>
             </div>
             </div>
